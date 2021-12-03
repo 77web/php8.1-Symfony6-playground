a80469790a704d68ea136d5af6732854e86802e0
77web <info@77-web.com>
2021-12-03 23:48:02 +0900

    InterestsをBackedEnumに変更

--- src/Domain/Enum/Interest.php
+++ src/Domain/Enum/Interest.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace App\Domain\Enum;
 
-enum Interest
+enum Interest: string
 {
-    case PHP;
-    case FRONTEND;
-    case INFRA;
+    case PHP = 'php';
+    case FRONTEND = 'angular';
+    case INFRA = 'aws';
 }
